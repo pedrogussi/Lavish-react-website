a577cfd4d8b779ad876d6054c516a54d954f7e44
pedrogussi <pedrogussi@hotmail.com>
2020-12-28 23:24:34 -0300

    28/12

--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import HeroSection from '../../HeroSection';
+
 import Pricing from '../../Pricing';
-import { homeObjOne} from './Data';
+
 
 function Services() {
     return (
         <>
-        <HeroSection {...homeObjOne} />
+        
         <Pricing/>
 
 
